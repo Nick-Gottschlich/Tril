625d9812fde6ff3f0500459f5ab965b771066f00
Nick Gottschlich <nickpgott@gmail.com>
2017-10-29 20:52:19 -0500

    V1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tril
+
+This is a chrome extension that swaps `@realDonaldTrump` and `@dril` tweets on twitter. Inspired by https://redd.it/5xnvn9
+
+# Where is this working
+
+- timeline
+- profile pages
+- individual tweets
+
+# Things to do
+
+- '_____ retweeted' isn't swapped
+- could probably make whole profile pages swap
+- change accounts in search bar
+- randomize function?
+
+# Why would you do this
+
+- install this on your "favorite" uncles computer and watch the fireworks
+- just for giggles
--- tril.js
+++ tril.js
@@ -1,21 +1,26 @@
-// @dril data-user-id: 16298441
-// @realDonaldTrump data-user-id: 25073877
-
+const drilID = '16298441';
+const trumpID = '25073877';
 const trumpAvatar = 'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_bigger.jpg';
+const drilAvatar = 'https://pbs.twimg.com/profile_images/847818629840228354/VXyQHfn0_bigger.jpg';
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations) {
-    // fired when a mutation occurs
-    console.log(mutations);
-
     // find the username of tweeters on the page (not @, so for @dril this is "wint")
     var tweetUsernameList = document.getElementsByClassName('fullname show-popup-with-id ');
     for (index in tweetUsernameList) {
-      // swap @dril tweets
-      if (tweetUsernameList[index].parentElement && tweetUsernameList[index].parentElement.parentElement.getAttribute("data-user-id") === '16298441') {
+      // if @dril tweets, swap for @realDonaldTrump
+      if (tweetUsernameList[index].parentElement && tweetUsernameList[index].parentElement.parentElement.getAttribute("data-user-id") === drilID) {
         if (tweetUsernameList[index].getAttribute("data-username-swapped") !== "true") {
           tweetUsernameList[index].innerHTML = 'Donald J. Trump';
+          tweetUsernameList[index].setAttribute("data-username-swapped", "true");
+        }
+      }
+
+      // if @realDonaldTrump tweets, swap for @dril
+      if (tweetUsernameList[index].parentElement && tweetUsernameList[index].parentElement.parentElement.getAttribute("data-user-id") === trumpID) {
+        if (tweetUsernameList[index].getAttribute("data-username-swapped") !== "true") {
+          tweetUsernameList[index].innerHTML = 'wint';
           tweetUsernameList[index].setAttribute("data-username-swapped", "true");
         }
       }
@@ -24,10 +29,18 @@ var observer = new MutationObserver(function(mutations) {
     // find the @handle or tweeters on the page (like @dril)
     var tweetHandleList = document.getElementsByClassName('username u-dir');
     for (index in tweetHandleList) {
-      // swap @dril tweets
-      if (tweetHandleList[index].parentElement && tweetHandleList[index].parentElement.getAttribute("data-user-id") === '16298441') {
+      // if @dril tweets, swap for @realDonaldTrump
+      if (tweetHandleList[index].parentElement && tweetHandleList[index].parentElement.getAttribute("data-user-id") === drilID) {
         if (tweetHandleList[index].getAttribute("data-handle-swapped") !== "true") {
           tweetHandleList[index].innerHTML = '@<b>realDonaldTrump</b>';
+          tweetHandleList[index].setAttribute("data-handle-swapped", "true");
+        }
+      }
+
+      // if @realDonaldTrump tweets, swap for @dril
+      if (tweetHandleList[index].parentElement && tweetHandleList[index].parentElement.getAttribute("data-user-id") === trumpID) {
+        if (tweetHandleList[index].getAttribute("data-handle-swapped") !== "true") {
+          tweetHandleList[index].innerHTML = '@<b>dril</b>';
           tweetHandleList[index].setAttribute("data-handle-swapped", "true");
         }
       }
@@ -36,24 +49,24 @@ var observer = new MutationObserver(function(mutations) {
     // find the avatars of tweeters on the page
     var tweetAvatarList = document.getElementsByClassName('avatar js-action-profile-avatar');
     for (index in tweetAvatarList) {
-      console.log('we in avatar')
-      // swap @dril tweets
-      if (tweetAvatarList[index].parentElement && tweetAvatarList[index].parentElement.getAttribute("data-user-id") === '16298441') {
+      // if @dril tweets, swap for @realDonaldTrump
+      if (tweetAvatarList[index].parentElement && tweetAvatarList[index].parentElement.getAttribute("data-user-id") === drilID) {
         if (tweetAvatarList[index].getAttribute("data-avatar-swapped") !== "true") {
-          console.log(tweetAvatarList[index])
-          console.log('setting avatar')
-          console.log(trumpAvatar)
           tweetAvatarList[index].setAttribute("src", trumpAvatar);
+          tweetAvatarList[index].setAttribute("data-avatar-swapped", "true");
+        }
+      }
+
+      // if @realDonaldTrump tweets, swap for @dril
+      if (tweetAvatarList[index].parentElement && tweetAvatarList[index].parentElement.getAttribute("data-user-id") === trumpID) {
+        if (tweetAvatarList[index].getAttribute("data-avatar-swapped") !== "true") {
+          tweetAvatarList[index].setAttribute("src", drilAvatar);
           tweetAvatarList[index].setAttribute("data-avatar-swapped", "true");
         }
       }
     }
 });
 
-console.log('Help Im trapped in the dev tools and cant find a way out');
-
-// geteElementById('timeline') will take care of the infinite scroll problem
-//  but I don't think it will take care of page change problems
 observer.observe(document, {
   attributes: false,
   childList: true,
